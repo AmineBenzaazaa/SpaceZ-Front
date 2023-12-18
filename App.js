7db67dbfa80cb59d0c69d60f069b19c229cfafd7
AftabUfaq <engr.aftabufaq@gmail.com>
2023-12-18 18:19:06 +0500

    new branch created

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Animated, Text, View } from "react-native";
-// import "react-native-gesture-handler";
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
